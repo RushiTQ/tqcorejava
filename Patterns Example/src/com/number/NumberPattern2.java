fc4108cd8ac7c24def3d023b6d8d6b25d78aa1e0
rushi <rushi@LAPTOP-CB617CVQ>
2023-04-10 09:39:32 +0530

    ok

---
+++ Patterns Example/src/com/number/NumberPattern2.java
@@ -0,0 +1,20 @@
+package com.number;
+
+public class NumberPattern2 {
+public static void main(String[] args) {
+		int n=5;
+		for(int i=n; i<=1; i--) {
+		
+		for(int j=n; j<=1; j--) {
+			
+			System.out.print(j);
+			
+		}
+		for(int j=n; j<=1; j--) {
+			System.out.println(" ");
+			
+		}
+			System.out.println();
+		}
+	}
+}
